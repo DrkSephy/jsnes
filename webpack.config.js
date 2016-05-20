342aa45389a0bd32fa911fc21516dba298a7f0a5
DrkSephy <dleonar00@citymail.cuny.edu>
2016-05-19 22:41:39 -0400

    Adding required libraries to webpack config for ES6 support

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,31 @@
 var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
-	entry: './source/main.js',
-	output: {
-		path: __dirname,
-		filename: 'bundle.js'
-	},
-	module: {
-		loaders: [
-			{
-				test: path.join(__dirname, 'source'),
-				loader: 'babel-loader'
-			}
-		]
-	}
+  entry: './source/main.js',
+  output: {
+    path: path.join(__dirname, 'source'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        test: path.join(__dirname, 'source'),
+        query: {
+          presets: 'es2015',
+        },
+      }
+    ]
+  },
+  plugins: [
+    // Avoid publishing files when compilation fails
+    new webpack.NoErrorsPlugin()
+  ],
+  stats: {
+    // Nice colored output
+    colors: true
+  },
+  // Create Sourcemaps for the bundle
+  devtool: 'source-map',
 };
