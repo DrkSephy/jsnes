1ac6be03a84b4dd21d098969fcdf979b8412ebca
DrkSephy <dleonar00@citymail.cuny.edu>
2016-05-19 22:42:23 -0400

    Starting cpu: configuring registers

---
+++ source/cpu.js
@@ -0,0 +1,14 @@
+class cpu {
+  constructor() {
+    this.r = {
+      a: 0,
+      x: 0,
+      y: 0,
+      s: 0xff,
+      p: 0,
+      pc: 0
+    }
+  }
+}
+
+export default cpu;
--- source/main.js
+++ source/main.js
@@ -1,1 +1,8 @@
+import cpu from './cpu.js'
+
+// console.log(cpu.r);
+
+let cp = new cpu();
+console.log(cp.r);
+
 console.log('Running Emulator');
