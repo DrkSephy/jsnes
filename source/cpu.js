37a9c376e8ad114284867e153f6a1fe43b4ef45b
DrkSephy <dleonar00@citymail.cuny.edu>
2016-05-22 22:02:06 -0400

    Adding status registers and flags

--- source/cpu.js
+++ source/cpu.js
@@ -1,12 +1,21 @@
 class cpu {
   constructor() {
     this.r = {
-      a: 0,     // Accumulator
-      x: 0,     // General Purpose X
-      y: 0,     // General Purpose Y
-      s: 0xff,  // Single-byte stack pointer
-      p: 0,     // Status Register (6 bits)
-      pc: 0     // Program Counter
+      a: 0,         // Accumulator
+      x: 0,         // General Purpose X
+      y: 0,         // General Purpose Y
+      s: 0xff,      // Single-byte stack pointer
+      p: 0b00100100,  // Status Register (6 bits)
+      pc: 0,        // Program Counter
+      flags: {
+        'N': 128,   // Negative
+        'V': 64,    // Overflow
+        'B': 16,    // Break
+        'D': 8,     // Decimal Mode
+        'I': 4,     // IRQ Disable
+        'Z': 2,     // Zero
+        'C': 1      // Carry
+      },
     }
   }
 
